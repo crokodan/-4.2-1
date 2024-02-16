606ee148dd9ea60793e937571111d1b55d53ddc9
Piholot2K (daniilhonda) <69081378+crokodan@users.noreply.github.com>
2024-02-16 19:06:48 +0400

    Add files via upload

---
+++ Задание 4.2 Базовый Вар 1/Задание 4.2 Базовый Вар 1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_4._2_Базовый_Вар_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int m = 3;
+            const int n = 4;
+            int[,] a = new int[m, n];
+            Random rand = new Random();
+            int i, j, k, x;
+
+            Console.WriteLine("Исходная матрица");
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    a[i, j] = rand.Next(100);
+                    Console.Write($"{a[i, j],4}");
+                }
+                Console.WriteLine();
+            }
+
+            for (i = 0; i < n - 1; i++)
+            {
+                for (j = i + 1; j < n; j++)
+                {
+                    if (a[m - 1, i] > a[m - 1, j])
+                    {
+                        for (k = 0; k < m; k++)
+                        {
+                            x = a[k, i];
+                            a[k, i] = a[k, j];
+                            a[k, j] = x;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Сортировка по возрастанию последней строки");
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    Console.Write($"{a[i, j],4}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
